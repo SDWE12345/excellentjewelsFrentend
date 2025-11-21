1c82ad9b1748aebc77892c74d70ea487951bc710
prashant-v-223 <prashantvadhvana@gmail.com>
2025-11-22 00:33:33 +0530

    ee

--- src/routes/index.js
+++ src/routes/index.js
@@ -71,6 +71,8 @@ const HipHopDetail = lazy(() => import('../Components/HipHopJewellery/HipHopDeta
 import PrivateRouter from './PrivateRouter';
 import Loader from 'Components/Global/Loader';
 import ChooseDiamond from 'Components/CustomizeRing/ChooseDiamond';
+import ChooseJewellery from 'Components/CustomizeRing/ChooseJewellery';
+import ChooseYourSetting from 'Components/CustomizeRing/ChooseYourSetting';
 
 const Index = () => {
   useEffect(() => {
@@ -88,10 +90,9 @@ const Index = () => {
     <Suspense fallback={<Loader/>}>
       <Header />
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/" element={<Home />} />
         <Route path="/diamond" element={<Diamond />} />
         <Route path="/diamond-detail" element={<DiamondDetail />} />
         <Route path="/compare" element={<Compare />} />
@@ -99,48 +100,111 @@ const Index = () => {
         <Route path="/cart" element={<ShoppingCart />} />
         <Route path="/jewellery" element={<Jewellery />} />
         <Route path="/jewellery-detail" element={<JewelleryDetail />} />
-        <Route path="/setting-jewellery-wise" element={<ChooseDiamond />} />
         <Route path="/hip-hop-jewellery" element={<HipHopJewellery />} />
         <Route path="/hip-hop-jewellery-detail" element={<HipHopDetail />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/share-demand" element={<DiamondInquiry />} />
-        <Route path="/events" element={<Events />} />
-
-        {/* Education Routes */}
-        <Route path="/education" element={<Education />} />
-        <Route path="/education/:id" element={<EducationDetail />} />
-        <Route path="/education/4cs-diamond" element={<FourCsDiamonds />} />
-        <Route path="/education/what-are-lab-grown-diamonds" element={<LabGrownDiamonds />} />
-        <Route path="/education/how-are-lab-grown-diamonds-created" element={<LabGrownDiamondsCreated />} />
-        <Route path="/education/chemical-vapor-deposition" element={<ChemicalVaporDeposition />} />
-        <Route path="/education/high-pressure-high-temperature" element={<HighPressureHighTemperature />} />
-        <Route path="/education/cvd-diamond-vs-hpht-diamond" element={<CVDvsHPHT />} />
-        <Route path="/education/natural-diamond-vs-lab-grown-diamond" element={<NaturalvsLabGrown />} />
-        <Route path="/education/diamond-mm-to-carat-weight-chats" element={<DiamondWeightChats />} />
-        <Route path="/education/advantages-of-lab-grown-diamonds" element={<AdvantagesofLabGrown />} />
-        <Route path="/education/choosing-the-perfect-engagement-ring" element={<ChoosingEngagementRing />} />
-        <Route path="/education/caring-for-your-lab-grown-diamond-jewelry" element={<CaringForLabGrownJewelry />} />
-
-        {/* Private Routes */}
-        <Route path="/edit-profile" element={<PrivateRouter><MyAccount2 /></PrivateRouter>} />
-        <Route path="/change-password" element={<PrivateRouter><ChangePassword /></PrivateRouter>} />
-        <Route path="/my-orders" element={<PrivateRouter><MyOrders /></PrivateRouter>} />
-        <Route path="/order-detail" element={<PrivateRouter><OrderDetail /></PrivateRouter>} />
+        <Route path="/setting-diamond-wise" element={<ChooseDiamond />} />
+        <Route path="/setting-jewellery-wise" element={<ChooseJewellery />} />
+        <Route path="/choose-your-setting" element={<ChooseYourSetting />} />
+        <Route
+          path="/edit-profile"
+          element={
+            <PrivateRouter>
+              <MyAccount2 />
+            </PrivateRouter>
+          }
+        />
+        <Route
+          path="/change-password"
+          element={
+            <PrivateRouter>
+              <ChangePassword />
+            </PrivateRouter>
+          }
+        />
+        <Route
+          path="/my-orders"
+          element={
+            <PrivateRouter>
+              <MyOrders />
+            </PrivateRouter>
+          }
+        />
+        <Route
+          path="/order-detail"
+          element={
+            <PrivateRouter>
+              <OrderDetail />
+            </PrivateRouter>
+          }
+        />
         <Route path="/tracker-my-order" element={<TrackMyOrder />} />
         <Route path="/purchase-history" element={<PurchaseHistory />} />
-        <Route path="/my-hold-list" element={<PrivateRouter><MyHoldList /></PrivateRouter>} />
-
-        {/* Policies */}
-        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-        <Route path="/shipping-policy" element={<ShippingPolicy />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/return-and-refund-policy" element={<ReturnAndRefundPolicy />} />
-
-        {/* Other Routes */}
+        <Route
+          path="/my-hold-list"
+          element={
+            <PrivateRouter>
+              <MyHoldList />
+            </PrivateRouter>
+          }
+        />
         <Route path="/contact-us" element={<ContactUs2 />} />
         <Route path="/parcel-goods" element={<ParcelGoods />} />
         <Route path="/parcel-goods-detail" element={<ParcelGoodsDetail />} />
         <Route path="/faqs" element={<Faq />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/education/:id" element={<EducationDetail />} />
+        <Route path="/education/4cs-diamond" element={<FourCsDiamonds />} />
+        <Route
+          path="/education/what-are-lab-grown-diamonds"
+          element={<LabGrownDiamonds />}
+        />
+        <Route
+          path="/education/how-are-lab-grown-diamonds-created"
+          element={<LabGrownDiamondsCreated />}
+        />
+        <Route
+          path="/education/chemical-vapor-deposition"
+          element={<ChemicalVaporDeposition />}
+        />
+        <Route
+          path="/education/high-pressure-high-temperature"
+          element={<HighPressureHighTemperature />}
+        />
+        <Route
+          path="/education/cvd-diamond-vs-hpht-diamond"
+          element={<CVDvsHPHT />}
+        />
+        <Route
+          path="/education/natural-diamond-vs-lab-grown-diamond"
+          element={<NaturalvsLabGrown />}
+        />
+        <Route
+          path="/education/diamond-mm-to-carat-weight-chats"
+          element={<DiamondWeightChats />}
+        />
+        <Route
+          path="/education/advantages-of-lab-grown-diamonds"
+          element={<AdvantagesofLabGrown />}
+        />
+        <Route
+          path="/education/choosing-the-perfect-engagement-ring"
+          element={<ChoosingEngagementRing />}
+        />
+        <Route
+          path="/education/caring-for-your-lab-grown-diamond-jewelry"
+          element={<CaringForLabGrownJewelry />}
+        />
+
+        <Route path="/about-us" element={<About />} />
+        <Route path="/share-demand" element={<DiamondInquiry />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/shipping-policy" element={<ShippingPolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route
+          path="/return-and-refund-policy"
+          element={<ReturnAndRefundPolicy />}
+        />
       </Routes>
       <Footer />
     </Suspense>
